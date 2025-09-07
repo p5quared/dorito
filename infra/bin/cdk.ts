495c37c1250a5172d33b9488b2acce205d054921
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-09-06 17:37:33 -0700

    chore: init savior service infra

--- infra/bin/cdk.ts
+++ infra/bin/cdk.ts
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DoritoStack } from '../lib/cdk-stack';
+import { SaviorStack } from '../lib/savior-stack';
+
+
 
 const app = new cdk.App();
+
+// Persistence Service
+const saviorStack = new SaviorStack(app, 'Savior');
+
+
+// Data Producer Service
 const imageTag = app.node.tryGetContext('imageTag');
-new DoritoStack(app, 'Dorito', { imageTag });
+new DoritoStack(app, 'Dorito', { 
+	imageTag,
+	saviorQueue: saviorStack.inputQueue 
+});
