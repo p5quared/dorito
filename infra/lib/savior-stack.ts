f2e3c9c03476a729e168fea8819dc84dde327fe2
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-09-07 12:19:33 -0700

    chore: init quicksave table

--- infra/lib/savior-stack.ts
+++ infra/lib/savior-stack.ts
@@ -1,16 +1,30 @@
-import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class SaviorStack extends Stack {
 	public readonly inputQueue: sqs.Queue;
 	public readonly lambda: lambda.Function;
+	public readonly table: dynamodb.Table;
 
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
+
+		this.table = new dynamodb.Table(this, 'QuickSaveTable', {
+			tableName: 'QuickSaveTable',
+			partitionKey: {
+				name: 'PK', type: dynamodb.AttributeType.STRING
+			},
+			sortKey: {
+				name: 'SK', type: dynamodb.AttributeType.STRING
+			},
+			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+			removalPolicy: RemovalPolicy.DESTROY,
+		});
 
 		this.inputQueue = new sqs.Queue(this, 'SaviorQueue', {
 			queueName: 'savior-queue',
@@ -18,7 +32,7 @@ export class SaviorStack extends Stack {
 			retentionPeriod: Duration.days(14),
 		});
 
-		this.lambda = new lambda.Function(this, 'SaviorLambda', {
+		this.lambda = new lambda.Function(this, 'QuickSave', {
 			runtime: lambda.Runtime.NODEJS_22_X,
 			handler: 'index.handler',
 			code: lambda.Code.fromAsset('../savior', {
@@ -33,6 +47,7 @@ export class SaviorStack extends Stack {
 			timeout: Duration.seconds(10),
 			environment: {
 				SQS_QUEUE_URL: this.inputQueue.queueUrl,
+				DYNAMODB_TABLE_NAME: this.table.tableName,
 			},
 		});
 
@@ -45,6 +60,17 @@ export class SaviorStack extends Stack {
 					'sqs:GetQueueAttributes',
 				],
 				resources: [this.inputQueue.queueArn],
+			})
+		);
+
+		this.lambda.addToRolePolicy(
+			new iam.PolicyStatement({
+				effect: iam.Effect.ALLOW,
+				actions: [
+					'dynamodb:PutItem',
+					'dynamodb:UpdateItem',
+				],
+				resources: [this.table.tableArn],
 			})
 		);
 
@@ -71,6 +97,18 @@ export class SaviorStack extends Stack {
 			value: this.lambda.functionName,
 			description: 'Savior Lambda Function Name',
 			exportName: `${id}-SaviorLambdaName`,
+		});
+
+		new CfnOutput(this, 'TableName', {
+			value: this.table.tableName,
+			description: 'Savior DynamoDB Table Name',
+			exportName: `${id}-SaviorTableName`,
+		});
+
+		new CfnOutput(this, 'TableArn', {
+			value: this.table.tableArn,
+			description: 'Savior DynamoDB Table ARN',
+			exportName: `${id}-SaviorTableArn`,
 		});
 	}
 }
