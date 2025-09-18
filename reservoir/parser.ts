6949159048471bd68b1fc1fece2aa73b03c1007a
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-09-17 21:02:41 -0700

    chore: remove unused

--- reservoir/parser.ts
+++ reservoir/parser.ts
@@ -4,19 +4,16 @@ import * as z from "zod"
 // We define the events that this handler will emit, 
 // because every handler handles one type of message, they will
 // only ever need to emit one type of event
-const EventTypeEnumSchema = z.enum([
-	"reservoir.raw",
-	"reservoir.keyword",
-	"trawl.raw",
-] as const)
+// const EventTypeEnumSchema = z.enum([
+// 	"reservoir.raw",
+// 	"reservoir.keyword",
+// 	"trawl.raw",
+// ] as const)
 
-export type EventType = z.infer<typeof EventTypeEnumSchema>
+// export type EventType = z.infer<typeof EventTypeEnumSchema>
 
 const EventMetadataSchema = z.object({
-	// INFO: Dont forget: this ID is what ties together all events
-	// for a given source whether post,comment, reddit, web, etc...
 	correlation_id: z.string(),
-	event_type: EventTypeEnumSchema,
 	source_date: z.string()
 })
 
