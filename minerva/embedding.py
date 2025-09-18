e31a88dd2fe91e954d2e7a0d6a1477ba377a874e
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-09-17 21:17:00 -0700

    feat: init embedding worker

---
+++ minerva/embedding.py
@@ -0,0 +1,6 @@
+class EmbeddingWorker:
+    def __init__(self, model_name: str = "sentence-transformers/all-MiniLM-L6-v2"):
+        from fastembed import TextEmbedding
+        self.model = TextEmbedding(model_name=model_name)
+    def embed(self, text: str):
+        return self.model.embed(text)
