1b420fdc3e001e05951a4b3b40de34e558e84694
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-09-20 12:57:53 -0700

    feat: init embedding worker and handler

--- reservoir/embedding.ts
+++ reservoir/embedding.ts
@@ -1,10 +1,16 @@
 import { eventHandler, MessageHandlerBuilder } from "./handling";
 import { EmbeddingProducedEvent, validateEmbeddingProducedEvent } from "./parser";
-import { printMessage } from "./utils";
+import { saveTopicVector } from "./s3vectors";
+import { getConfigRequired, printMessage } from "./utils";
+
+const saveTopicEmbeddingToS3Vector = async (e: EmbeddingProducedEvent) => {
+  await saveTopicVector(getConfigRequired('VECTOR_BUCKET_NAME'), getConfigRequired('VECTOR_INDEX_NAME'))(e.data.keyword, e.data.vector)
+}
 
 const EmbeddingHandler = new MessageHandlerBuilder<EmbeddingProducedEvent>()
 	.withValidator(validateEmbeddingProducedEvent)
 	.withHandler(printMessage)
+	.withHandler(saveTopicEmbeddingToS3Vector)
 	.build()
 
 const EmbeddingEventHandler = eventHandler<EmbeddingProducedEvent>(EmbeddingHandler.handle.bind(EmbeddingHandler))
